fa83153dfe05ca7fab33ddeb265439dc451bb3eb
Iburinoc <iburinoc@gmail.com>
2014-06-22 09:49:03 -0700

    Works.  yey.  now to stop sending my password over http

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http'),
-      fs = require('fs');
+      fs = require('fs'),
+    querystring = require('querystring'),
+    exec = require('child_process').exec;
 
 var server = http.createServer(handler);
 
@@ -14,8 +16,27 @@ function handler(req, res) {
 		res.write(page);
 		res.end();
 	} else if(req.method == 'POST') {
+		var fullbody = '';
 		req.on('data', function(chunk) {
-			console.log('data:' + chunk);
+			//console.log('data:' + chunk);
+			fullbody += chunk;
+		});
+		req.on('end', function() {
+			var data = querystring.parse(fullbody);
+			console.log(data.pwname);
+			exec('echo "' + data.mpw + '\n" | spass get ' + data.pwname,
+				function(err, stdout, stderr) {
+					if(err) {
+						res.writeHead(500);
+						res.end();
+						return;
+					}
+					res.writeHead(200, {'Content-Type': 'text/plain'});
+					res.write(stdout);
+					res.write(stderr);
+					res.end();
+				}
+			);
 		});
 	}
 }
