633fb618b9aa8fc90b871b677c87221a1fcbdab0
Sean Purcell <me@seanp.xyz>
2017-12-03 13:15:50 -0500

    Get server working

--- server.py
+++ server.py
@@ -1,14 +1,40 @@
 #!/usr/bin/python3
 
 import subprocess
+import sys
 
-from flask import Flask
+from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='')
+
+SPASS='/usr/local/bin/spass'
 
 def call_spass(name, master):
-    subprocess.Popen(
+    p = subprocess.Popen([
+            SPASS,
+            'get',
+            name],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        close_fds=True)
+    try:
+        out, err = p.communicate(bytes('%s\n' % master, 'utf8'), timeout=5)
+    except subprocess.TimeoutExpired:
+        p.kill()
+        return 'Error: spass process timedout'
+
+    return out if p.returncode == 0 else err
 
 @app.route('/getpwd', methods=['POST'])
 def getpwd():
-    
+    return call_spass(request.form['name'], request.form['master'])
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        SPASS = sys.argv[1]
+    app.run()
