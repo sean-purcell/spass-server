d9f70d52b7579aa0020dfe52024763825b408d1f
Sean Purcell <me@seanp.xyz>
2017-12-31 03:18:17 +0000

    Add rate limiting

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 
 from flask import Flask, request
+from flask_limiter import Limiter
 
 app = Flask(__name__)
 
@@ -12,6 +13,16 @@ SPASS=sys.argv[1] if len(sys.argv) > 1 else 'spass'
 DBFILE=sys.argv[2] if len(sys.argv) > 2 else ''
 
 COMMAND = [SPASS, '-s'] + (['-d', DBFILE] if DBFILE else [])
+
+def get_client_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+        return request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        return request.remote_addr
+
+limiter = Limiter(app,
+    key_func=get_client_ip,
+    default_limits=["1/second", "10/minute", "100/day"])
 
 @app.before_first_request
 def setup():
@@ -37,13 +48,13 @@ def call_spass(stdin, cmd):
 def get():
     val, code = call_spass(request.form['master'], ['get',
         request.form['name']])
-    app.logger.info('get: %s %s %d', request.remote_addr, request.form['name'], code)
+    app.logger.info('get: %s %s %d', get_client_ip(), request.form['name'], code)
     return val
 
 @app.route('/spass/ls', methods=['POST'])
 def ls():
     val, code = call_spass(request.form['master'], ['ls'])
-    app.logger.info('ls: %s %d', request.remote_addr, code)
+    app.logger.info('ls: %s %d', get_client_ip(), code)
     return val.replace(b'\n', b'<br/>')
 
 @app.route('/spass/gen', methods=['POST'])
@@ -59,7 +70,7 @@ def gen():
             '-A', C('upper'),
             '-0', C('digit'),
             '-@', C('sym')])
-    app.logger.info('gen: %s %s %d', request.remote_addr, request.form['name'], code)
+    app.logger.info('gen: %s %s %d', get_client_ip(), request.form['name'], code)
     return val.replace(b'\n', b'<br/>')
 
 @app.route('/spass/add', methods=['POST'])
@@ -68,7 +79,7 @@ def add():
         '%s\n%s\n%s' % (request.form['master'], request.form['password'],
             request.form['confirm']),
         ['add', request.form['name']])
-    app.logger.info('add: %s %s %d', request.remote_addr, request.form['name'], code)
+    app.logger.info('add: %s %s %d', get_client_ip(), request.form['name'], code)
     return val
 
 @app.route('/spass/rm', methods=['POST'])
@@ -76,7 +87,7 @@ def rm():
     val, code = call_spass(
         '%s' % (request.form['master']),
         ['rm', request.form['name']])
-    app.logger.info('rm: %s %s %d', request.remote_addr, request.form['name'], code)
+    app.logger.info('rm: %s %s %d', get_client_ip(), request.form['name'], code)
     return val
 
 @app.route('/spass/chpw', methods=['POST'])
@@ -85,7 +96,7 @@ def chpw():
         '%s\n%s\n%s' % (request.form['master'], request.form['newmaster'],
             request.form['confirm']),
         ['chpw'])
-    app.logger.info('chpw: %s %d', request.remote_addr, code)
+    app.logger.info('chpw: %s %d', get_client_ip(), code)
     return val
 
 @app.route('/')
